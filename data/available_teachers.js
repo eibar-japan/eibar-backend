8696af9601632fe9bdeead8947a1461244065b7d
Ian Cameron <ian.kameron@gmail.com>
2019-05-28 16:40:03 +0900

    update teacher data, adding id

--- data/available_teachers.js
+++ data/available_teachers.js
@@ -5,12 +5,14 @@ module.exports = [
       rating: 4,
       long: 139.727908,
       lat: 35.660890,
+      teacher_id: "0006"
     },
     {
       name: "Shane McDougal",
       rating: 2,
       long: 139.730671,
       lat: 35.662167,
+      teacher_id: "0004"
     },
   ],
   [
@@ -19,12 +21,14 @@ module.exports = [
       rating: 1,
       long: 139.728870,
       lat: 35.660644,
+      teacher_id: "0072"
     },
     {
       name: "Bunter Lostrung",
       rating: 5,
       long: 139.728243, 
       lat: 35.661011,
+      teacher_id: "0068"
     },
   ]
 ]
