ad458194731ebf3f4ac6457ad4d5371db3575bee
Ian Cameron <ian.kameron@gmail.com>
2021-10-15 09:10:05 +0900

    update tests for new api routing, auth endpoints

--- test/api/user/userDbTest.js
+++ test/api/user/userDbTest.js
@@ -5,13 +5,15 @@ const chaiHttp = require("chai-http");
 const { v4: v4uuid, validate: validate_uuid } = require("uuid");
 const { userFactory } = require("../../../api/user/userUtil");
 const { ERROR_DICT } = require("../../../api/util/error_handling");
+const badInput = require("./userUnitTest");
 
 chai.use(chaiHttp);
 
 const { knex, app } = require("../../../index");
 
-describe("User CRUD", () => {
-  // CREATE
+describe("Operations on existing users", () => {
+  // NOTE: registration and login is covered in the Auth test file.
+
   let INPUT_USER_DATA;
 
   beforeEach(async () => {
@@ -19,105 +21,127 @@ describe("User CRUD", () => {
       email: "test4@example.com",
       first_name: "jane",
       last_name: "brewer",
+      password: "Aa1aaaaa",
     };
     return knex("eibaruser").delete();
   });
 
-  it("should create a new user in the database for unused email address", () => {
-    return chai
-      .request(app)
-      .post("/api/user")
-      .send(INPUT_USER_DATA)
-      .then((res) => {
-        assert.equal(res.status, 200);
-        assert.typeOf(res.body, "Object");
+  // UPDATE
 
-        return knex("eibaruser")
-          .select()
-          .where({ email: INPUT_USER_DATA.email });
-      })
-      .then((rows) => {
-        assert.equal(rows.length, 1);
-        const CREATED_USER = rows[0];
-        assert.equal(CREATED_USER.email, INPUT_USER_DATA.email);
-        assert.equal(CREATED_USER.first_name, INPUT_USER_DATA.first_name);
-        assert.equal(CREATED_USER.last_name, INPUT_USER_DATA.last_name);
-      });
-  });
-
-  it("should create a new user in the database if email is available post-deletion", () => {
+  it("should update existing users in the database (one at a time)", () => {
     let userEid = "";
+    const newEmail = "newemail@test.com";
+    const newFirstName = "newFirstName";
+    const newLastName = "newLastName";
     const requester = chai.request(app).keepOpen();
     // Create User
     return requester
-      .post("/api/user")
+      .post("/api/register")
       .send(INPUT_USER_DATA)
       .then((res) => {
-        assert.equal(res.status, 200);
         userEid = res.body.eid;
-        // Delete user
-        return requester.delete(`/api/user/${userEid}`);
+        patchBody = { first_name: newFirstName };
+        return requester.patch(`/api/user/${userEid}`).send(patchBody);
       })
       .then((res) => {
-        assert.equal(res.status, 204);
-
-        // Create again with names different but same email
-        SECOND_INPUT_USER_DATA = {
-          ...INPUT_USER_DATA,
-          first_name: "John",
-          last_name: "Other",
-        };
-        return requester.post("/api/user").send(SECOND_INPUT_USER_DATA);
+        assert.equal(res.body.first_name, newFirstName);
+        patchBody = { last_name: newLastName };
+        return requester.patch(`/api/user/${userEid}`).send(patchBody);
       })
       .then((res) => {
-        assert.equal(res.status, 200);
-        assert.typeOf(res.body, "Object");
-
-        // Get user data from DB for checks
-        return knex("eibaruser")
-          .select()
-          .where({ email: INPUT_USER_DATA.email })
-          .orderBy("created_at");
+        assert.equal(res.body.last_name, newLastName);
+        patchBody = { email: newEmail };
+        return requester.patch(`/api/user/${userEid}`).send(patchBody);
+      })
+      .then((res) => {
+        assert.equal(res.body.email, newEmail);
+        return knex("eibaruser").select().where({ eid: userEid });
       })
       .then((rows) => {
-        // 2 users exist (one with deleted_at, one without)
-        const FIRST_USER = rows[0];
-        const SECOND_USER = rows[1];
-
-        assert.equal(rows.length, 2);
-        assert.isNotNull(FIRST_USER.deleted_at);
-        assert.isNull(SECOND_USER.deleted_at);
-        assert.equal(SECOND_USER.email, SECOND_INPUT_USER_DATA.email);
-        assert.equal(SECOND_USER.first_name, SECOND_INPUT_USER_DATA.first_name);
-        assert.equal(SECOND_USER.last_name, SECOND_INPUT_USER_DATA.last_name);
+        assert.isNull(rows[0].deleted_at);
+        assert.equal(rows[0].first_name, newFirstName);
+        assert.equal(rows[0].last_name, newLastName);
+        assert.equal(rows[0].email, newEmail);
       });
   });
 
-  it("should fail to create a new user in the database if email is existing", () => {
+  it("should update existing users in the database (multiple fields at once)", () => {
     let userEid = "";
-    const requester = chai.request(app).keepOpen();
+    const newFirstName = "newFirstName";
+    const newLastName = "newLastName";
 
+    const requester = chai.request(app).keepOpen();
+    // Create User
     return requester
-      .post("/api/user")
+      .post("/api/register")
       .send(INPUT_USER_DATA)
       .then((res) => {
-        assert.equal(res.status, 200);
         userEid = res.body.eid;
-        // try to create same user again
-        return requester.post("/api/user").send(INPUT_USER_DATA);
+        patchBody = { first_name: newFirstName, last_name: newLastName };
+        return requester.patch(`/api/user/${userEid}`).send(patchBody);
+      })
+      .then((res) => {
+        assert.equal(res.body.first_name, newFirstName);
+        assert.equal(res.body.last_name, newLastName);
+        return knex("eibaruser").select().where({ eid: userEid });
+      })
+      .then((rows) => {
+        assert.isNull(rows[0].deleted_at);
+        assert.equal(rows[0].first_name, newFirstName);
+        assert.equal(rows[0].last_name, newLastName);
+        assert.equal(rows[0].email, INPUT_USER_DATA.email);
+      });
+  });
+
+  it("should fail to update existing users in the database (bad input)", () => {
+    let userEid = "";
+    const newEmail = badInput.TOO_LONG_EMAIL();
+    const newFirstName = badInput.TOO_LONG_FIRST_NAME();
+    const newLastName = badInput.TOO_LONG_LAST_NAME();
+    const requester = chai.request(app).keepOpen();
+    // Create User
+    return requester
+      .post("/api/register")
+      .send(INPUT_USER_DATA)
+      .then((res) => {
+        userEid = res.body.eid;
+        patchBody = { first_name: newFirstName };
+        return requester.patch(`/api/user/${userEid}`).send(patchBody);
       })
       .then((res) => {
         assert.equal(res.status, 400);
-        assert.equal(
-          res.body.errors[0].code,
-          ERROR_DICT.E0007_USER_EXISTS.error.code
+        assert.deepEqual(
+          res.body.errors[0],
+          ERROR_DICT.E0002_USER_FIRST_NAME_LONG.error
         );
+        patchBody = { last_name: newLastName };
+        return requester.patch(`/api/user/${userEid}`).send(patchBody);
+      })
+      .then((res) => {
+        assert.equal(res.status, 400);
+        assert.deepEqual(
+          res.body.errors[0],
+          ERROR_DICT.E0004_USER_LAST_NAME_LONG.error
+        );
+        patchBody = { email: newEmail };
+        return requester.patch(`/api/user/${userEid}`).send(patchBody);
+      })
+      .then((res) => {
+        assert.equal(res.status, 400);
+        assert.deepEqual(
+          res.body.errors[0],
+          ERROR_DICT.E0005_USER_EMAIL_LONG.error
+        );
+
+        return knex("eibaruser").select().where({ eid: userEid });
+      })
+      .then((rows) => {
+        assert.isNull(rows[0].deleted_at);
+        assert.equal(rows[0].first_name, INPUT_USER_DATA.first_name);
+        assert.equal(rows[0].last_name, INPUT_USER_DATA.last_name);
+        assert.equal(rows[0].email, INPUT_USER_DATA.email);
       });
   });
-
-  // UPDATE
-
-  it("should update existing users in the database");
 
   // DELETE
 
@@ -126,7 +150,7 @@ describe("User CRUD", () => {
     const requester = chai.request(app).keepOpen();
     // Create User
     return requester
-      .post("/api/user")
+      .post("/api/register")
       .send(INPUT_USER_DATA)
       .then((res) => {
         userEid = res.body.eid;
@@ -150,7 +174,7 @@ describe("User CRUD", () => {
     const requester = chai.request(app).keepOpen();
     // Create User
     return requester
-      .post("/api/user")
+      .post("/api/register")
       .send(INPUT_USER_DATA)
       .then((res) => {
         const badEID = "INVALID_ID_NOT_UUID_FORMAT";
@@ -167,7 +191,7 @@ describe("User CRUD", () => {
     const requester = chai.request(app).keepOpen();
     // Create User
     return requester
-      .post("/api/user")
+      .post("/api/register")
       .send(INPUT_USER_DATA)
       .then((res) => {
         userEid = res.body.eid;
@@ -188,7 +212,7 @@ describe("User CRUD", () => {
     const requester = chai.request(app).keepOpen();
     // Create User
     return requester
-      .post("/api/user")
+      .post("/api/register")
       .send(INPUT_USER_DATA)
       .then((res) => {
         userEID = res.body.eid;
